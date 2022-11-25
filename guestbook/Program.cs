55375e1dc7556a8117b870be4bb5efe91304677a
Joel - joli2114 <jooel.lindh@gmail.com>
2022-11-25 11:58:23 +0100

    first commit

---
+++ guestbook/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using System.Text.Json;
+
+namespace guestbook
+{
+    public class Post
+    {
+        // sökväg för fil
+        private string filename = @"posts.json";
+
+        private string user1;
+        public string message1;
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
