fc764e43241c54971b597959d0f36a58a11d7c56
Joel - joli2114 <jooel.lindh@gmail.com>
2022-11-26 04:46:25 +0100

    added basic switch choises

--- guestbook/Program.cs
+++ guestbook/Program.cs
@@ -41,27 +41,64 @@ namespace guestbook
         {
             // inom <> finsn det som ska lagras, objekt för json
             List <object> poster = new List <object> ();
-            Console.WriteLine("theja");
-            
 
 
-            // testa filsökväg
-            if (File.Exists(@"C:\Users\46703\source\repos\guestbook\guestbook/posts.json") == true) {
-                Console.WriteLine("Filsökväg hittad");
-                string jsonString = File.ReadAllText(@"C:\Users\46703\source\repos\guestbook\guestbook/posts.json");
-                // deserialisera som objekt
-                poster = JsonSerializer.Deserialize<List<object>>(jsonString);
 
-                foreach (object a in poster)
-                {
-                    Console.WriteLine(a);
+            // frågeställning för alternativ
+
+            Console.WriteLine("Välkommen till gästboken \n");
+
+            Console.WriteLine("Gör ett val nedan \n\n");
+
+            Console.WriteLine("1 för att visa alla poster \n");
+            Console.WriteLine("X för att avsluta \n");
+
+
+            // readkey är samma som readline men man slipper enter
+            int input = (int)Console.ReadKey(true).Key;
+            switch (input) {
+                case '1':
+                    printPosts();
+                    break;
+                case '2':
+                    Console.WriteLine("Du har valt 2");
+                    break;
+
+                case 88:
+                    Environment.Exit(0);
+                    break;
+            }
+
+
+
+
+
+
+
+
+
+
+            void printPosts()
+            {
+                // testa filsökväg
+                if (File.Exists(@"C:\Users\46703\source\repos\guestbook\guestbook/posts.json") == true)
+                {   // spara i lokal variabel
+                    Console.WriteLine("Filsökväg hittad");
+                    string jsonString = File.ReadAllText(@"C:\Users\46703\source\repos\guestbook\guestbook/posts.json");
+                    // deserialisera som objekt
+                    poster = JsonSerializer.Deserialize<List<object>>(jsonString);
+
+                    // skriv ut
+                    foreach (object a in poster)
+                    {
+                        Console.WriteLine(a);
+                    }
                 }
-
+                else // om inte hittat fil
+                {
+                    Console.WriteLine("\nhittade inte filen");
+                }
             }
-            else {
-                Console.WriteLine("\nhittade itne filen");
-            }
-
             
 
 
